b2209ff2711f1c9fb8a7934fed4949ef94e136a2
ahaggi <a.haggi@yahoo.com>
2016-06-14 22:17:41 +0200

    5

--- MultiThread/src/modell/L01_DefiningThreadOgStartTips.java
+++ MultiThread/src/modell/L01_DefiningThreadOgStartTips.java
@@ -1,35 +1,35 @@
 package modell;
 
-public class l01 {
+public class L01_DefiningThreadOgStartTips {
 
 	public static void main(String[] args) throws InterruptedException {
 
 		Mythread t1 = new Mythread();
 		t1.start();
-
-//        Thread.sleep(4000);
-        
-		for (int i = 0; i < 10; i++) {
-			System.out.println("main thread00000000000000000000000000000000000000");
-			
-		}
+/** .start():
+ * 1-registerer den nye thread på schedule
+ * 2-perform all other mandatory activities
+ * 3-invoke run method */
 		
+/**OBS vi kan bruke t1.run() uten start(), men da lager det ikke en ny tråd. 
+ * den blir kjørt som vanlig objekt-metode*/
 
+/**ved kjøring av neste kode ,vil det skrive ut main thread/child thread  eller child thread/main thread
+ * eller blanding*/
+
+/***/
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("main thread00000000000000000000000000000000000000");			
+		}
 	}
-
-	
-	
-	
-	
-	
-	
 }
 
 class Mythread extends Thread {
 	@Override
 	public void run(){
 		for (int i = 0; i < 10; i++) {
-			System.out.println("child thread----------------------------------");
+			System.out.println("child thread--------------------------------------");
 		}
 	}
 }
